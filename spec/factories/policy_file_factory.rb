66f29a3952a6773f1c65ead0b95be2a92a0da81f
Elana Kopelevich <ekopelevich@gmail.com>
2020-09-13 14:26:42 -0600

    tests PolicyFile attachement

---
+++ spec/factories/policy_file_factory.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :policy_file do
+    name { "name" }
+    after(:build) do |policy_file|
+      policy_file.data.attach(
+        io: File.open(Rails.root.join('spec', 'csv', 'test_policies.csv')),
+        filename: 'test_policies.csv',
+        content_type: 'text/csv'
+      )
+    end
+  end
+end
--- spec/models/policy_file_spec.rb
+++ spec/models/policy_file_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PolicyFile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "has a valid data attachment" do
+    @policy_file = create(:policy_file)
+    expect(@policy_file.data).to be_an_instance_of(ActiveStorage::Attached::One) 
+  end
 end
